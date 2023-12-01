5029525161eb511fe62ebbde27316cc26aec516f
ttt161 <losto@nix>
2023-12-01 17:57:35 +0300

    TD-814: cleanup, bump mg_core

--- src/machinegun_health_check.erl
+++ src/machinegun_health_check.erl
@@ -13,26 +13,22 @@ consuela() ->
 
 -spec global() -> {erl_health:status(), erl_health:details()}.
 global() ->
-    logger:warning("list nodes: ~p", [nodes()]),
-    ReplicaCount = os:getenv("REPLICA_COUNT", "1"),
-    ClusterSize = erlang:list_to_integer(ReplicaCount),
+    ClusterSize = mg_core_union:cluster_size(),
     ConnectedCount = erlang:length(erlang:nodes()),
     case is_quorum(ClusterSize, ConnectedCount) of
         true ->
             {passing, []};
         false ->
             Reason =
-                <<"no quorum. cluster size: ", (erlang:list_to_binary(ReplicaCount))/binary, ", connected: ",
-                    (erlang:integer_to_binary(ConnectedCount))/binary>>,
+                <<"union. no quorum. cluster size: ", (erlang:integer_to_binary(ClusterSize))/binary, ", online: ",
+                    (erlang:integer_to_binary(ConnectedCount + 1))/binary>>,
             {critical, Reason}
     end.
 
 -spec startup() -> {erl_health:status(), erl_health:details()}.
 startup() ->
-    ClusterOpts = application:get_env(machinegun, cluster, undefined),
-    Addrs = inet:getaddrs("machinegun-ha-headless", inet),
-    logger:warning("MG_DEBUG: Startup with opts: ~p", [ClusterOpts]),
-    logger:warning("MG_DEBUG: Startup resolved: ~p", [Addrs]),
+    %% maybe any checks?
+    logger:info("union. node ~p started", [node()]),
     {passing, []}.
 
 %% Internal functions
